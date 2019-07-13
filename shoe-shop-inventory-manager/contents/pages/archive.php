ca20c8428807d0ea6a36abb4a9ea43d187225f9a
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-07-13 22:07:33 +0300

    Fix archive page title name

--- shoe-shop-inventory-manager/contents/pages/archive.php
+++ shoe-shop-inventory-manager/contents/pages/archive.php
@@ -8,7 +8,7 @@ include_once '../includes/header.php';
 <div id="page-wrapper">
     <div class="row" style="padding-top:30px;">
         <div class="col-lg-6">
-            <h1 class="page-header">Shipments</h1>
+            <h1 class="page-header">Archive</h1>
         </div>
     </div>
 </div>
