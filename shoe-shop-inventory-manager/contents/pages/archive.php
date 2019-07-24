01545fff9a55b396709bb2c68a1a042182525b44
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-07-24 22:18:12 +0300

    Add method for nullifying tables

--- shoe-shop-inventory-manager/contents/pages/archive.php
+++ shoe-shop-inventory-manager/contents/pages/archive.php
@@ -45,71 +45,75 @@ include_once('../includes/auth_validate.php');
     }
     
     if (isset($_POST['export'])) { 
-		 $DB_HOST=DB_HOST;
-    $DB_USER=DB_USER;
-    $DB_PASS=DB_PASSWORD;
-    $DB_NAME=DB_NAME;
-    
-    $conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
-    $conn->set_charset("utf8");
-    
-    $tables = array();
-    $sql = "SHOW TABLES";
-    $result = mysqli_query($conn, $sql);
-    
-    while ($row = mysqli_fetch_row($result)) {
-        $tables[] = $row[0];
-    }
-    
-    $sqlScript = "";
-    foreach ($tables as $table) {
-        
-        $query = "SHOW CREATE TABLE $table";
-        $result = mysqli_query($conn, $query);
-        $row = mysqli_fetch_row($result);
-        
-        $sqlScript .= "\n\n" . $row[1] . ";\n\n";
-        $query = "SELECT * FROM $table";
-        $result = mysqli_query($conn, $query);
-        
-        $columnCount = mysqli_num_fields($result);
-        for ($i = 0; $i < $columnCount; $i ++) {
-            while ($row = mysqli_fetch_row($result)) {
-                $sqlScript .= "INSERT INTO $table VALUES(";
-                for ($j = 0; $j < $columnCount; $j ++) {
-                    $row[$j] = $row[$j];
-                    
-                    if (isset($row[$j])) {
-                        $sqlScript .= '"' . $row[$j] . '"';
-                    } else {
-                        $sqlScript .= '""';
-                    }
-                    if ($j < ($columnCount - 1)) {
-                        $sqlScript .= ',';
-                    }
-                }
-                $sqlScript .= ");\n";
-            }
-        }
-        
-        $sqlScript .= "\n";
-    }
-    
-    if(!empty($sqlScript))
-    {        
-        $backup_file_name = $DB_NAME . '_backup_' . $timenow . '.sql';
-        $fileHandler = fopen($backup_file_name, 'w+');
-        $number_of_lines = fwrite($fileHandler, $sqlScript);
-        fclose($fileHandler);
-        copy($backup_file_name,'backup/'.$backup_file_name);
-        unlink($backup_file_name);
+		$DB_HOST=DB_HOST;
+		$DB_USER=DB_USER;
+		$DB_PASS=DB_PASSWORD;
+		$DB_NAME=DB_NAME;
+		
+		$conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+		$conn->set_charset("utf8");
+		
+		$tables = array();
+		$sql = "SHOW TABLES";
+		$result = mysqli_query($conn, $sql);
+		
+		while ($row = mysqli_fetch_row($result)) {
+			$tables[] = $row[0];
+		}
+		
+		$sqlScript = "";
+		foreach ($tables as $table) {
+			
+			$query = "SHOW CREATE TABLE $table";
+			$result = mysqli_query($conn, $query);
+			$row = mysqli_fetch_row($result);
+			
+			$sqlScript .= "\n\n" . $row[1] . ";\n\n";
+			$query = "SELECT * FROM $table";
+			$result = mysqli_query($conn, $query);
+			
+			$columnCount = mysqli_num_fields($result);
+			for ($i = 0; $i < $columnCount; $i ++) {
+				while ($row = mysqli_fetch_row($result)) {
+					$sqlScript .= "INSERT INTO $table VALUES(";
+					for ($j = 0; $j < $columnCount; $j ++) {
+						$row[$j] = $row[$j];
+						
+						if (isset($row[$j])) {
+							$sqlScript .= '"' . $row[$j] . '"';
+						} else {
+							$sqlScript .= '""';
+						}
+						if ($j < ($columnCount - 1)) {
+							$sqlScript .= ',';
+						}
+					}
+					$sqlScript .= ");\n";
+				}
+			}
+			
+			$sqlScript .= "\n";
+		}
+		
+		if(!empty($sqlScript))
+		{        
+			$backup_file_name = $DB_NAME . '_backup_' . $timenow . '.sql';
+			$fileHandler = fopen($backup_file_name, 'w+');
+			$number_of_lines = fwrite($fileHandler, $sqlScript);
+			fclose($fileHandler);
+			copy($backup_file_name,'backup/'.$backup_file_name);
+			unlink($backup_file_name);
 
-        echo '<script language="javascript">';
-        echo 'alert("Database '.$backup_file_name.' is saved successfully!")';
-        echo '</script>';
-        header("Refresh:0");
-    }   
+			echo '<script language="javascript">';
+			echo 'alert("Database '.$backup_file_name.' is saved successfully!")';
+			echo '</script>';
+			header("Refresh:0");
+		}   
     }
+	
+	if(isset($_POST['nullify'])) 
+		echo 'nullify';
+	}
 
 include_once '../includes/header.php';
 ?>
